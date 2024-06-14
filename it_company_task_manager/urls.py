0928d49170dfb9225835e01de31be3ee7928a33f
Maksym Nimych <maksimnimitch@gmail.com>
2024-06-14 21:07:42 +0300

    Add an opportunity to login and logout via sidebar and create templates for the actions

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
